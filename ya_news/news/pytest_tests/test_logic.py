d858da3e1e37369c4baccb63fc9af9eefb3240e9
Tony <r4zeel@yandex.ru>
2023-11-06 16:20:43 +0300

    added bad words test to test_logic

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -1,7 +1,9 @@
-from pytest_django.asserts import assertRedirects
+from pytest_django.asserts import assertRedirects, assertFormError
+
 from django.urls import reverse
 
 from news.models import Comment
+from news.forms import BAD_WORDS, WARNING
 
 from http import HTTPStatus
 
@@ -16,10 +18,11 @@ def test_user_can_create_comment(author_client, author, form_data, news):
     new_comment = Comment.objects.get()
     assert new_comment.text == form_data['text']
     assert new_comment.author == author
+    assert new_comment.news == news
 
 
 @pytest.mark.django_db
-def test_anonymous_user_cant_create_note(client, form_data, news):
+def test_anonymous_user_cant_create_comment(client, form_data, news):
     url = reverse('news:detail', args=(news.pk,))
     response = client.post(url, data=form_data)
     login_url = reverse('users:login')
@@ -37,7 +40,7 @@ def test_author_can_edit_comment(author_client, form_data, comment):
     assert comment.text == form_data['text']
 
 
-def test_other_user_cant_edit_note(admin_client, form_data, comment):
+def test_other_user_cant_edit_comment(admin_client, form_data, comment):
     url = reverse('news:edit', args=(comment.pk,))
     response = admin_client.post(url, form_data)
     assert response.status_code == HTTPStatus.NOT_FOUND
@@ -45,7 +48,7 @@ def test_other_user_cant_edit_note(admin_client, form_data, comment):
     assert comment.text == comment_from_db.text
 
 
-def test_author_can_delete_note(author_client, comment, comments_pk_for_args):
+def test_author_can_delete_comment(author_client, comment, comments_pk_for_args):
     url = reverse('news:delete', args=(comments_pk_for_args,))
     success_url = reverse('news:detail', args=(comment.news_id,))
     response = author_client.post(url)
@@ -53,7 +56,7 @@ def test_author_can_delete_note(author_client, comment, comments_pk_for_args):
     assert Comment.objects.count() == 0
 
 
-def test_other_user_cant_delete_note(
+def test_other_user_cant_delete_comment(
         admin_client,
         form_data,
         comments_pk_for_args
@@ -62,3 +65,13 @@ def test_other_user_cant_delete_note(
     response = admin_client.post(url)
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert Comment.objects.count() == 1
+
+
+def test_user_cant_use_bad_words(author_client, news):
+    comments_count_before_post = Comment.objects.count()
+    bad_words_data = {'text': f'Какой-то текст, {BAD_WORDS[0]}, еще текст'}
+    url = reverse('news:detail', args=(news.pk,))
+    response = author_client.post(url, data=bad_words_data)
+    assertFormError(response, 'form', 'text', errors=WARNING)
+    comments_count = Comment.objects.count()
+    assert comments_count == comments_count_before_post
